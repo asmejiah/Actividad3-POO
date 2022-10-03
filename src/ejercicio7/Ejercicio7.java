9455f013d9c1802f0ebc2625d507adc40d9a3331
Asdruval Mejia <Asdruval Mejia@LAPTOP-J5VJCCQV>
2022-10-02 21:54:02 -0500

    Updated

---
+++ src/ejercicio7/Ejercicio7.java
@@ -0,0 +1,10 @@
+package ejercicio7;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        Formulario fr = new Formulario();
+        fr.setVisible(true);
+    }
+    
+}
