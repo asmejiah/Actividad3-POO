bd91bb7af9131fe1ee9eee538386fbf6f1c33afd
asmejiah <111844634+asmejiah@users.noreply.github.com>
2022-10-02 22:50:35 -0500

    Add files via upload

---
+++ Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,10 @@
+package ejercicio10;
+
+public class Ejercicio10 {
+
+    public static void main(String[] args) {
+        Formulario fr=new Formulario();
+        fr.setVisible(true);
+    }
+    
+}
